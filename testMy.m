b796ab01b5baef9a5106acd8ae6c48e2739c7f36
Martijn Aben <maben@mathworks.com>
2022-02-08 11:03:23 +0100

    Test secrets

--- testMy.m
+++ testMy.m
@@ -4,7 +4,9 @@ classdef testMy < matlab.unittest.TestCase
             testCase.verifyTrue(true)
         end
         function secondTest(testCase)
-            testCase.verifyTrue(false)
+            testCase.verifyNotEmpty(getenv('secret1'));
+            testCase.verifyNotEmpty(getenv('secret2'));
+            testCase.verifyEqual(getenv('secret1'),getenv('secret2'))
         end
     end
 
