8f7fca006f760431f939ee6c677021790619f9cb
Martijn Aben <maben@mathworks.com>
2022-02-08 10:45:05 +0100

    Testing a failure

--- testMy.m
+++ testMy.m
@@ -3,6 +3,9 @@ classdef testMy < matlab.unittest.TestCase
         function simpleTest(testCase)
             testCase.verifyTrue(true)
         end
+        function secondTest(testCase)
+            testCase.verifyTrue(false)
+        end
     end
 
 end
